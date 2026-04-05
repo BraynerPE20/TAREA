074d14516658bca32860e762271c9fe0d21a9f9a
Brayner <braynerparedes97@gmail.com>
2026-04-05 17:41:27 -0500

    Los ejercicios para la tarea

--- Trabajo/Los ejercios/Main12.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upeu.trabajo;
-
-public class Main12 {
-    public static void main(String[] args) {
-        Ejercicio12 l = new Ejercicio12();
-        l.agregar(1);
-        l.agregar(2);
-    }
-}
--- Trabajo/Los ejercios/Main9.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upeu.trabajo;
-
-public class Main9 {
-    public static void main(String[] args) {
-        Ejercicio9 v = new Ejercicio9("Toyota",200);
-        v.describir();
-    }
-}
