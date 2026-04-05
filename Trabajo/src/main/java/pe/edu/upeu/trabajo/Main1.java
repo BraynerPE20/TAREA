d4c709469d0b682f16ac36e412765e858ecd8ab9
Brayner <braynerparedes97@gmail.com>
2026-04-05 17:31:18 -0500

    Agrego ejercicio 1

--- Trabajo/src/main/java/pe/edu/upeu/trabajo/Main1.java
+++ Trabajo/src/main/java/pe/edu/upeu/trabajo/Main1.java
@@ -1,6 +1,6 @@
 package pe.edu.upeu.trabajo;
 
-public class Main {
+public class Main1 {
     public static void main(String[] args) {
         Ejercicio1 p1 = new Ejercicio1("Brayner", 20, "brayner@gmail.com");
         Ejercicio1 p2 = new Ejercicio1("Luis", 25, "luis@gmail.com");
